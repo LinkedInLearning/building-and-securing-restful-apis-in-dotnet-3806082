fbf348dfbfac58a1c364323f3a7a4b19623fc123
matt Milner <matt.milner@m3technologypartners.com>
2024-04-09 22:55:00 -0500

    01-06e

--- LiL.TimeTracking/Models/TimeEntry.cs
+++ LiL.TimeTracking/Models/TimeEntry.cs
@@ -1,11 +1,19 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace LiL.TimeTracking.Models;
 
 public class TimeEntry
 {
     public Guid Id  { get; set; }
 
+    public int EmployeeId { get; set; }
+
+    [ForeignKey(nameof(EmployeeId))]
     public virtual Employee Employee { get; set; }
 
+    public int ProjectId { get; set; }
+
+    [ForeignKey(nameof(ProjectId))] 
     public virtual Project Project {get;set;}
 
     public DateOnly DateWorked { get; set; }
